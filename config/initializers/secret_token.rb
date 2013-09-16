3e61a5059d27f96c00f3ed63749f26a92b33c1be
kimyee <kimyee.16@gmail.com>
2013-09-16 15:44:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainforestPart2::Application.config.secret_token = '9273b5602c73f81d43371df14b8a4eaf060d7323daefa3177f812922a238390e4ef00ca28df5d486d4f29a2c11e2d393c78940236adb3d3cf3fd31f76cfdbba8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RainforestPart2::Application.config.session_store :cookie_store, key: '_rainforest_part2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RainforestPart2::Application.config.session_store :active_record_store
